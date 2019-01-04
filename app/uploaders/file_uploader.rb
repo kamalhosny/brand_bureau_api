b75254afe23ce68528e959f36a2053ff0f71b6ce
kamalhosny <kamal-aldin-hosny@hotmail.com>
2019-01-05 00:44:59 +0200

    add seeded test user

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -16,22 +16,9 @@ class FileUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Create different versions of your uploaded files:
-  # version :thumb, if: :image? do
-  #   process resize_to_fit: [50, 50]
-  # end
-
-  # version :medium, if: :image? do
-  #   process resize_to_fit: [200, 200]
-  # end
-
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
     %w[jpg jpeg png svg 3gp mp4 wmv rmvb]
   end
-
-  # def image?(new_file)
-  #   new_file.content_type.start_with? 'image'
-  # end
 end
